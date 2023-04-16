30cd5d2ac55ac2cafebc01f9b0dc7a0c3ae1e100
FarazRashid <muhammadfarazrashid@gmail.com>
2023-04-16 19:57:04 +0500

    still trying to fix q4 and failing

--- IndividualQueries/Faraz/farazQueries.sql
+++ IndividualQueries/Faraz/farazQueries.sql
@@ -1,16 +1,17 @@
 
---query 4
+--query 4 teams with foreign managers
 
 
-select Teams.TEAM_NAME
+select Teams.TEAM_NAME, teams.ID, concat(Managers.FIRST_NAME, ' ' ,Managers.LAST_NAME) AS ManagerName, Managers.NATIONALITY
 from Teams
 inner join Stadiums on Teams.HOME_STADIUM_ID=Stadiums.ID
 inner join Location on Location.CITY=Stadiums.CITY
 inner join Managers on Managers.TEAM_ID=TEAM_ID
-where Managers.NATIONALITY=Location.COUNTRY;
+where Managers.NATIONALITY!=Location.COUNTRY
+group by Teams.ID, TEAM_NAME, Managers.FIRST_NAME, Managers.LAST_NAME, Managers.NATIONALITY;
 
-
---query 5
+--query 5 stadiums having capacity greater than 60,000
+ 
 select Matches.MATCH_ID, Stadiums.NAME,Stadiums.CAPACITY from
 Matches
 join Stadiums on Matches.STADIUM_ID=Stadiums.ID
