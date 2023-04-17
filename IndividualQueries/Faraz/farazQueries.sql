f7c9ed3d93405e7dc134c57be7d8facbed0ad934
FarazRashid <muhammadfarazrashid@gmail.com>
2023-04-17 08:14:37 +0500

    Update farazQueries.sql

--- IndividualQueries/Faraz/farazQueries.sql
+++ IndividualQueries/Faraz/farazQueries.sql
@@ -7,8 +7,7 @@ from Teams
 inner join Stadiums on Teams.HOME_STADIUM_ID=Stadiums.ID
 inner join Location on Location.CITY=Stadiums.CITY
 inner join Managers on Managers.TEAM_ID=TEAM_ID
-where Managers.NATIONALITY!=Location.COUNTRY
-group by Teams.ID, TEAM_NAME, Managers.FIRST_NAME, Managers.LAST_NAME, Managers.NATIONALITY;
+where Managers.NATIONALITY!=Location.COUNTRY and Managers.TEAM_ID= teams.ID;
 
 --query 5 stadiums having capacity greater than 60,000
  
@@ -18,5 +17,16 @@ join Stadiums on Matches.STADIUM_ID=Stadiums.ID
 where Stadiums.CAPACITY>60000
  order by stadiums.CAPACITY asc;
 
- --query 
+ --query 9 season with greatest number of left foot goals
+
+
+ select top 1 Matches.SEASON, count ( goals.GOAL_ID) as leftFootedGoals  
+ from Matches
+ inner join Goals on Matches.MATCH_ID=Goals.MATCH_ID
+ where GOAL_DESC='left-footed shot'
+ group by Matches.SEASON 
+ order by leftFootedGoals desc;
+
+
+ --query 10
 
